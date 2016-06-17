07e0994723997b2b60070e302a1cecd643b50a77
Fabien Steines <fabien.steines@post.lu>
2016-06-17 17:16:57 +0200

    working on layout

--- include/javascript/ignis.js
+++ include/javascript/ignis.js
@@ -3,167 +3,170 @@
  */
 'use strict';
 
-// create an new instance of a pixi stage
-var stage = new PIXI.Stage(0x66FF99);
-stage.interactive = true;
+$(function(){
+    // create an new instance of a pixi stage
+    var stage = new PIXI.Stage(0x66FF99);
+    stage.interactive = true;
 
-stage.on('mousedown', clickEvent);
-stage.on('touchstart', clickEvent);
-stage.on("mousemove",moveEvent);
+    stage.on('mousedown', clickEvent);
+    stage.on('touchstart', clickEvent);
+    stage.on("mousemove",moveEvent);
 
 
 // create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, getCanvasHeight(),{antialias: false, transparent: false});
+    var renderer = PIXI.autoDetectRenderer(window.innerWidth, getCanvasHeight(),{antialias: false, transparent: false});
 
 // add the renderer view element to the DOM
-document.body.appendChild(renderer.view);
+    document.body.appendChild(renderer.view);
 
-requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 // create a texture from an image path
-var texture01 = PIXI.Texture.fromImage("resource/image/tile01.png");
-var texture02 = PIXI.Texture.fromImage("resource/image/tile02.png");
-var texture03 = PIXI.Texture.fromImage("resource/image/tile03.png");
+    var texture01 = PIXI.Texture.fromImage("../resource/image/tile01.png");
+    var texture02 = PIXI.Texture.fromImage("../resource/image/tile02.png");
+    var texture03 = PIXI.Texture.fromImage("../resource/image/tile03.png");
 
-var texture04 = PIXI.Texture.fromImage("resource/image/cave.png");
+    var texture04 = PIXI.Texture.fromImage("../resource/image/cave.png");
 
-var tile4 = new PIXI.Sprite(texture04);
+    var tile4 = new PIXI.Sprite(texture04);
 
-var mousePosition;
-var cellSize = 32;
+    var mousePosition;
+    var cellSize = 32;
 
 
-var stats = new Stats();
+    var stats = new Stats();
 //document.body.appendChild( stats.domElement );
 
-stats.domElement.style.position = "absolute";
-stats.domElement.style.top = "0px";
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.top = "0px";
 
-tile4.anchor.x = 0.5;
-tile4.anchor.y = 0.5;
-tile4.position.x = 500;
-tile4.position.y = 500;
+    tile4.anchor.x = 0.5;
+    tile4.anchor.y = 0.5;
+    tile4.position.x = 500;
+    tile4.position.y = 500;
 
 // Initialize the pixi Graphics class
-var cursor = new PIXI.Graphics();
+    var cursor = new PIXI.Graphics();
 // Set the fill color
-cursor.beginFill(0xe74c3c); // Red
-cursor.drawRect(0,0,32,32);
-cursor.endFill();
-cursor.alpha = 0.75;
-buildTileMap();
+    cursor.beginFill(0xe74c3c); // Red
+    cursor.drawRect(0,0,32,32);
+    cursor.endFill();
+    cursor.alpha = 0.75;
+    buildTileMap();
 
 
-function animate() {
+    function animate() {
 
-    stats.begin();
-    requestAnimationFrame(animate);
-    /*
-     // just for fun, lets rotate mr rabbit a little
-     tileset.rotation += 0.1;
-     */
+        stats.begin();
+        requestAnimationFrame(animate);
+        /*
+         // just for fun, lets rotate mr rabbit a little
+         tileset.rotation += 0.1;
+         */
 
-    if (tile4 != null) {
-        tile4.rotation += 0.1;
+        if (tile4 != null) {
+            tile4.rotation += 0.1;
+        }
+
+
+        // render the stage
+        renderer.render(stage);
+        stats.end();
     }
 
+    /**
+     * Build the Tilemap
+     */
+    function buildTileMap() {
 
-    // render the stage
-    renderer.render(stage);
-    stats.end();
-}
+        stage.removeChildren(0, stage.children.length);
 
-/**
- * Build the Tilemap
- */
-function buildTileMap() {
+        cursor.width = cellSize;
+        cursor.height = cellSize;
 
-    stage.removeChildren(0, stage.children.length);
+        var w = window.innerWidth;
+        var h = getCanvasHeight();
 
-    cursor.width = cellSize;
-    cursor.height = cellSize;
+        var maxX = w / cellSize;
+        var maxY = h / cellSize;
 
-    var w = window.innerWidth;
-    var h = getCanvasHeight();
+        for (var i = 0; i < maxX; i++) {
+            for (var j = 0; j < maxY; j++) {
 
-    var maxX = w / cellSize;
-    var maxY = h / cellSize;
+                var tile = new PIXI.Sprite(texture01);
 
-    for (var i = 0; i < maxX; i++) {
-        for (var j = 0; j < maxY; j++) {
+                tile.anchor.x = 0;
+                tile.anchor.y = 0;
+                tile.position.x = cellSize * i;
+                tile.position.y = cellSize * j;
+                tile.width = cellSize;
+                tile.height = cellSize;
+                tile.tint = 0x999999;
 
-            var tile = new PIXI.Sprite(texture01);
+                var tile2 = new PIXI.Sprite(texture02);
 
-            tile.anchor.x = 0;
-            tile.anchor.y = 0;
-            tile.position.x = cellSize * i;
-            tile.position.y = cellSize * j;
-            tile.width = cellSize;
-            tile.height = cellSize;
-            tile.tint = 0x999999;
+                tile2.anchor.x = 0;
+                tile2.anchor.y = 0;
+                tile2.position.x = cellSize * i;
+                tile2.position.y = cellSize * j;
+                tile2.width = cellSize;
+                tile2.height = cellSize;
 
-            var tile2 = new PIXI.Sprite(texture02);
+                var tile3 = new PIXI.Sprite(texture03);
 
-            tile2.anchor.x = 0;
-            tile2.anchor.y = 0;
-            tile2.position.x = cellSize * i;
-            tile2.position.y = cellSize * j;
-            tile2.width = cellSize;
-            tile2.height = cellSize;
+                tile3.anchor.x = 0;
+                tile3.anchor.y = 0;
+                tile3.position.x = cellSize * i;
+                tile3.position.y = cellSize * j;
+                tile3.width = cellSize;
+                tile3.height = cellSize;
+                tile3.alpha = 0.5;
 
-            var tile3 = new PIXI.Sprite(texture03);
+                stage.addChild(tile);
+                stage.addChild(tile2);
+                stage.addChild(tile3);
 
-            tile3.anchor.x = 0;
-            tile3.anchor.y = 0;
-            tile3.position.x = cellSize * i;
-            tile3.position.y = cellSize * j;
-            tile3.width = cellSize;
-            tile3.height = cellSize;
-            tile3.alpha = 0.5;
+            }
+        }
 
-            stage.addChild(tile);
-            stage.addChild(tile2);
-            stage.addChild(tile3);
+        //stage.addChild(tile4);
+        stage.addChild(cursor);
+    }
 
+    window.onresize = function (event){
+        var w = window.innerWidth;
+        var h = getCanvasHeight();
+
+        //this part resizes the canvas but keeps ratio the same
+        renderer.view.style.width = w + "px";
+        renderer.view.style.height = h + "px";
+
+        //this part adjusts the ratio:
+        renderer.resize(w,h);
+        buildTileMap();
+    }
+
+    function clickEvent(event) {
+        console.log("Mouse Click @ {X:" + mousePosition.x + " Y:" + mousePosition.y +"}");
+    }
+
+    function moveEvent(event) {
+        mousePosition = event.data.global;
+
+        var posX = Math.floor(mousePosition.x / cellSize) * cellSize;
+        var posY = Math.floor(mousePosition.y / cellSize) * cellSize;
+
+        if (posX != cursor.x || posY != cursor.y) {
+            cursor.x = posX;
+            cursor.y = posY;
         }
     }
 
-    //stage.addChild(tile4);
-    stage.addChild(cursor);
-}
-
-window.onresize = function (event){
-    var w = window.innerWidth;
-    var h = getCanvasHeight();
-
-    //this part resizes the canvas but keeps ratio the same
-    renderer.view.style.width = w + "px";
-    renderer.view.style.height = h + "px";
-
-    //this part adjusts the ratio:
-    renderer.resize(w,h);
-    buildTileMap();
-}
-
-function clickEvent(event) {
-    console.log("Mouse Click @ {X:" + mousePosition.x + " Y:" + mousePosition.y +"}");
-}
-
-function moveEvent(event) {
-    mousePosition = event.data.global;
-
-    var posX = Math.floor(mousePosition.x / cellSize) * cellSize;
-    var posY = Math.floor(mousePosition.y / cellSize) * cellSize;
-
-    if (posX != cursor.x || posY != cursor.y) {
-        cursor.x = posX;
-        cursor.y = posY;
+    function getCanvasWidth() {
+        return 0;
     }
-}
 
-function getCanvasWidth() {
-    return 0;
-}
+    function getCanvasHeight() {
+        return window.innerHeight;// - $("#toolbar").height() - 1; // -1 from Border
+    }
 
-function getCanvasHeight() {
-    return window.innerHeight - 32; //- $("#toolbar").height;
-}
+})
