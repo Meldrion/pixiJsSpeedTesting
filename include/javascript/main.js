bf1f4507917847a8704efbbe313e9b32727d2038
Fabien Steines <fabien.steines@post.lu>
2016-06-17 10:17:27 +0200

    added icon

--- include/javascript/main.js
+++ include/javascript/main.js
@@ -20,7 +20,7 @@
 
     app.on('ready', function () {
 
-        var mainWindow = new BrowserWindow({width:800, height:600});
+        var mainWindow = new BrowserWindow({width:800, height:600,icon: __dirname + '../../../resource/icon/ignis.png'});
         console.log('file://' + __dirname + '/index.html');
         mainWindow.loadURL('file://' + __dirname + '../../../index.html');
         app.on('window-all-closed', () => {
