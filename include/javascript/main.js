01bc737ebfac46000069a5f035c6d81ae2e83680
Fabien Steines <fabien.steines@post.lu>
2016-06-17 10:03:10 +0200

    Electron package bugfixes

--- include/javascript/main.js
+++ include/javascript/main.js
@@ -2,19 +2,40 @@
  * Created by fabien on 17.06.16.
  */
 
-(function(){
+//(function(){
 
-    var application = require('app'),
-        BrowserWindow = require('browser-window');
+    /*
+    //var application = require('app'),
+    //    BrowserWindow = require('browser-window');
+    const application = require('electron')
+    const BrowserWindow = require('electron')
+    */
 
+
+
+    const {app} = require('electron');
+    const {BrowserWindow} = require('electron');
+
+
+
+    app.on('ready', function () {
+
+        var mainWindow = new BrowserWindow({width:800, height:600});
+        console.log('file://' + __dirname + '/index.html');
+        mainWindow.loadURL('file://' + __dirname + '../../../index.html');
+        app.on('window-all-closed', () => {
+            app.quit();
+        });
+    });
+    /*
     application.on('ready', function() {
 
         var mainWindow = new BrowserWindow({width:800, height:600});
-        mainWindow.loadUrl('file://' + __dirname + '/index.html');
+        //mainWindow.loadUrl('file://' + __dirname + '/index.html');
         mainWindow.on('closed', function() {
             mainWindow = null;
         });
 
     });
 
-})();
+})();*/
